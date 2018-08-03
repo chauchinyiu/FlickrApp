e5aaa74aca4c99fb4b845d214470a700d3cfd1c3
Chau Chin Yiu <chinyiu.chau@quartett-mobile.de>
2018-08-03 13:36:08 +0200

    initial setup

---
+++ Framework/simulator/LittleFramework.framework/Headers/LittleFramework.h
@@ -0,0 +1,19 @@
+//
+//  LittleFramework.h
+//  LittleFramework
+//
+//  Created by Chau Chin Yiu on 01.08.18.
+//  Copyright © 2018 Quartett mobile GmbH. All rights reserved.
+//
+@import Foundation;
+#import <UIKit/UIKit.h>
+
+//! Project version number for LittleFramework.
+FOUNDATION_EXPORT double LittleFrameworkVersionNumber;
+
+//! Project version string for LittleFramework.
+FOUNDATION_EXPORT const unsigned char LittleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LittleFramework/PublicHeader.h>
+
+
