d6b92d1506b21f395d65635c14354ee91ecf5117
Chau Chin Yiu <chinyiu.chau@quartett-mobile.de>
2018-08-03 13:38:56 +0200

    Tableviewcell for each section

---
+++ FlickrApp/Cell/PhotosTableViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  TableViewCell.swift
+//  FlickrApp
+//
+//  Created by Chau Chin Yiu on 31/7/2018.
+//  Copyright © 2018 Chau Chin Yiu. All rights reserved.
+//
+
+import UIKit
+import LittleFramework
+
+class PhotosTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    var collectionViewCellViewModels : [PhotoCollectionViewCellViewModel] = []
+}
+
+extension PhotosTableViewCell {
+ 
+    func updateCollectionViewContent(viewModels: [PhotoCollectionViewCellViewModel]){
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionViewCellViewModels = viewModels
+        collectionView.reloadData()
+    }
+}
+
+extension PhotosTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return collectionViewCellViewModels.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell : PhotoCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCollectionViewCell", for: indexPath) as! PhotoCollectionViewCell
+ 
+        let cellViewModel: PhotoCollectionViewCellViewModel = collectionViewCellViewModels[indexPath.item]
+        cell.bind(to:cellViewModel)
+        return cell
+    }
+    
+}
