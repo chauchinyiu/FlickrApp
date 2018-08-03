c49428b22bf04cc198772d1fc293d9d71beb2ac3
Chau Chin Yiu <chinyiu.chau@quartett-mobile.de>
2018-08-03 13:39:28 +0200

    collectionviewcellviewmodel

---
+++ FlickrApp/ViewModel/PhotoCollectionViewCellViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  ImageCellViewModel.swift
+//  FlickrApp
+//
+//  Created by Chau Chin Yiu on 2/8/2018.
+//  Copyright © 2018 Chau Chin Yiu. All rights reserved.
+//
+
+import UIKit
+import LittleFramework
+
+class PhotoCollectionViewCellViewModel: ViewModel {
+    let photoID = Bindable<String?>(nil)
+    let photoName = Bindable<String?>(nil)
+    let photoImageUrl = Bindable<URL?>(nil)
+    let photoImage = Bindable<UIImage?>(nil)
+    
+    let flickrPhoto: FlickrPhoto
+    init(photo: FlickrPhoto) {
+        flickrPhoto = photo
+        photoID.value = photo.photoId
+        photoName.value = photo.title
+        photoImageUrl.value = photo.photoUrl
+        FlickrConnector.loadImage(url: photo.photoUrl) { (error, image) in
+            if image != nil{
+                self.photoImage.value = image
+            }
+        }
+    }
+}
