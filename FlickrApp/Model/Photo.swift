5a02a835834a94caac8643469484e4db34ab4ce4
Chau Chin Yiu <chinyiu.chau@quartett-mobile.de>
2018-08-05 01:25:25 +0200

    rename

--- FlickrApp/Model/Photo.swift
+++ FlickrApp/Model/Photo.swift
@@ -16,6 +16,11 @@ struct Photo {
     let server: String
     let title: String
     let description: String
+    let dateUploadedInmMillisecond : String
+    let dateOfTaken : String
+    let ownerUserName : String
+    let tags : String
+    
     var photoUrl: URL {
         return URL(string: "https://farm\(farm).staticflickr.com/\(server)/\(photoId)_\(secret)_m.jpg")!
     }
@@ -32,9 +37,39 @@ struct Photo {
           title = photoDictionary["title"] as? String ?? ""
            if let content = photoDictionary["description"] as? NSDictionary,
               let text = content["_content"] as? String {
-             description = text
+              description = text
            }else{
-            description = ""
+              description = ""
             }
+           dateUploadedInmMillisecond = photoDictionary["dateuploaded"] as? String ?? ""
+          if let owner = photoDictionary["owner"] as? NSDictionary,
+             let username = owner["username"] as? String {
+                ownerUserName = username
+          }else {
+            ownerUserName = ""
+         }
+        if let dates = photoDictionary["dates"] as? NSDictionary,
+            let taken = dates["taken"] as? String {
+            dateOfTaken = taken
+        }else {
+            dateOfTaken = "unknown"
+        }
+         if let tagsContainer  = photoDictionary["tags"] as? NSDictionary,
+            let tagsArray = tagsContainer["tag"] as?  [NSDictionary] {
+            var tagStrings: String = ""
+            for tagDictionary in tagsArray{
+                if let content = tagDictionary["_content"] as? String{
+                    var space = " #"
+                    if tagStrings == "" {
+                        space = "#"
+                    }
+                    tagStrings = tagStrings.appending(space).appending(content)
+                }
+                
+            }
+            tags = tagStrings
+        }else {
+            tags = ""
+        }
     }
 }
