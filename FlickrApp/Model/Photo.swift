5055bb133f7f23fee9b8550d4c6c11c97f1f8d85
Chau Chin Yiu <chinyiu.chau@quartett-mobile.de>
2018-08-05 15:58:04 +0200

    change data model

--- FlickrApp/Model/Photo.swift
+++ FlickrApp/Model/Photo.swift
@@ -16,11 +16,13 @@ struct Photo {
     let server: String
     let title: String
     let description: String
-    let dateUploadedInmMillisecond : String
+    let dateUploaded : String
     let dateOfTaken : String
     let ownerUserName : String
     let tags : String
+ 
     
+
     var photoUrl: URL {
         return URL(string: "https://farm\(farm).staticflickr.com/\(server)/\(photoId)_\(secret)_m.jpg")!
     }
@@ -34,14 +36,27 @@ struct Photo {
           farm = photoDictionary["farm"] as? Int ?? 0
           secret = photoDictionary["secret"] as? String ?? ""
           server = photoDictionary["server"] as? String ?? ""
-          title = photoDictionary["title"] as? String ?? ""
+        if let text = photoDictionary["title"] as? String {
+             title = text
+        } else if let dict = photoDictionary["title"] as? NSDictionary, let content = dict["_content"] as? String {
+                title = content
+        }else{
+            title = ""
+        }
            if let content = photoDictionary["description"] as? NSDictionary,
               let text = content["_content"] as? String {
               description = text
            }else{
               description = ""
             }
-           dateUploadedInmMillisecond = photoDictionary["dateuploaded"] as? String ?? ""
+        if let millisecond  = photoDictionary["dateuploaded"] as? String,
+            let timeinterval = Double(millisecond){
+             let dateString = UtilitiesTool.dateFormatter().string(from: Date(timeIntervalSince1970: (timeinterval))) 
+            self.dateUploaded = dateString
+        }else{
+            self.dateUploaded = "unknown"
+        }
+     
           if let owner = photoDictionary["owner"] as? NSDictionary,
              let username = owner["username"] as? String {
                 ownerUserName = username
