be3c237757eeee9b60bd635169cd3fcca7a8af81
Chau Chin Yiu <chinyiu.chau@quartett-mobile.de>
2018-08-05 15:57:17 +0200

    move functions to utilitestool

---
+++ FlickrApp/Utilities/UtilitiesTool.swift
@@ -0,0 +1,73 @@
+//
+//  Helper.swift
+//  FlickrApp
+//
+//  Created by Chau Chin Yiu on 5/8/2018.
+//  Copyright © 2018 Chau Chin Yiu. All rights reserved.
+//
+
+import UIKit
+
+class UtilitiesTool  {
+    
+    
+    static func share(image: UIImage,  in viewController: UIViewController){
+        // image to share
+        let image = image
+        
+        // set up activity view controller
+        let imageToShare = [image]
+        let activityViewController = UIActivityViewController(activityItems: imageToShare, applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = viewController.view // so that iPads won't crash
+        
+        // exclude some activity types from the list (optional)
+        activityViewController.excludedActivityTypes = [ UIActivityType.airDrop, UIActivityType.postToFacebook ]
+        
+        // present the view controller
+        viewController.present(activityViewController, animated: true, completion: nil)
+    }
+    
+ 
+    static func saveImageInDocuments(image: UIImage, name: String = "default", in viewController: UIViewController)   {
+     
+        guard let data = UIImageJPEGRepresentation(image, 1) ?? UIImagePNGRepresentation(image) else {
+            showAlert(message: "Failed to save in Documents!", in: viewController)
+            return
+        }
+        guard let directory = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false) as NSURL else {
+            showAlert(message: "Failed to save in Documents!", in: viewController)
+            return
+        }
+        do {
+            
+            let fileName = "\(name).png"
+            try data.write(to: directory.appendingPathComponent(fileName)!)
+            showAlert(message: "Successfully saved", in: viewController)
+        } catch {
+            print(error.localizedDescription)
+            showAlert(message: "Failed to save in Documents!", in: viewController)
+        }
+    }
+    
+     static func openImageInBrowser(imageURL: URL){
+ 
+        UIApplication.shared.open( imageURL, options: [:], completionHandler: { (status) in
+            
+        })
+    }
+    
+    static func showAlert(message:String, in viewController: UIViewController) {
+        let alertController = UIAlertController(title: "Flickr App", message:
+            message, preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+        
+        viewController.present(alertController, animated: true, completion: nil)
+    }
+
+    static func dateFormatter() -> DateFormatter{
+      let formatter = DateFormatter()
+      formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+      return formatter
+    }
+
+}
